a9f383c4d513b52c0871d1d7354488d89ddff4c3
AQMujtaba <37418444+AQMujtaba@users.noreply.github.com>
2018-07-21 13:30:18 -0400

    Implemented main to read cmd line args

--- main.cc
+++ main.cc
@@ -1,10 +1,34 @@
 #include "quadris.h"
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-int main(){
-	
-  Quadris myGame{false, 1, "sequence.txt", 0};
+int main(int args, char *argv[]) {
+  int seed = 1; // default random sequence
+  int level = 0; // default starting level
+  bool textOnly = false; // default state with no "-text"
+  string file = "sequence.txt"; // defaut script file
+
+  for (int i = 1; i < args; ++i) {
+  	string arg = argv[i];
+  	if (arg == "-text") {
+  		textOnly = true;
+  	} else if (arg == "-seed") {
+  		stringstream in(argv[i + 1]);
+  		if (!(in >> seed)) { // check for successful read
+  			seed = 1; // back to default
+  		}
+  	} else if (arg == "-scriptfile") {
+  		file = argv[i + 1];
+  	} else if (arg == "-startlevel") {
+  		stringstream in(argv[i + 1]);
+  		if (!(in >> level)) { // check for successful read
+  			level = 0; // back to default
+  		}
+  	}
+  }
+
+  Quadris myGame{textOnly, seed, file, level};
   myGame.Start();
 }
