8b35a664259067b4e7c06fe4f59a275a3242ec5e
AQMujtaba <37418444+AQMujtaba@users.noreply.github.com>
2018-07-21 17:51:30 -0400

    Modified main to check file

--- main.cc
+++ main.cc
@@ -8,7 +8,7 @@ int main(int args, char *argv[]) {
   int seed = 1; // default random sequence
   int level = 0; // default starting level
   bool textOnly = false; // default state with no "-text"
-  string file = "sequence.txt"; // defaut script file
+  string fileName = "sequence.txt"; // defaut script file
 
   for (int i = 1; i < args; ++i) {
   	string arg = argv[i];
@@ -20,7 +20,7 @@ int main(int args, char *argv[]) {
   			seed = 1; // back to default
   		}
   	} else if (arg == "-scriptfile") {
-  		file = argv[i + 1];
+  		fileName = argv[i + 1];
   	} else if (arg == "-startlevel") {
   		stringstream in(argv[i + 1]);
   		if (!(in >> level)) { // check for successful read
@@ -29,6 +29,18 @@ int main(int args, char *argv[]) {
   	}
   }
 
+  ifstream file(fileName);
+  if (!(file.good())) { // check if file exists/readable
+  	cerr << "Please provide a readable file." << endl;
+  	return 1;
+  }
+
   Quadris myGame{textOnly, seed, file, level};
-  myGame.Start();
+  
+  try {
+  	myGame.Start();
+  }
+  catch (...) {
+  	return 1;
+  }
 }
